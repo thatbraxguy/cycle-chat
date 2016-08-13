420eb9bd3e1f8d9bf04bcc7baff11eb64c7cf68c
Braxton Frederick <bfrederick@constantcontact.com>
2016-08-13 10:27:23 -0400

    Setup simple cycle app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import Cycle from '@cycle/core';
 import { makeDOMDriver } from '@cycle/dom';
+import { div, p, input } from '@cycle/dom';
 import { Observable } from 'rx';
 
 function main(sources) {
+  const sinks = {
+    DOM: sources.DOM
+      .select('input')
+      .events('click')
+      .map(e => e.target.checked)
+      .startWith(false)
+      .map(toggled =>
+        div([
+          input({ attrs: { type: 'checkbox' } }),
+          'Toggle me',
+          p(toggled ? 'ON' : 'OFF')
+        ])
+      )
+  };
 
+  return sinks;
 };
 
-function drivers() {
+const drivers = {
   DOM: makeDOMDriver('#app')
 };
+
+Cycle.run(main, drivers);
